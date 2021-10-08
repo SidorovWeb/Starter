79d79671991f6eb4a397d17567a417b801ef1447
Alexandr <sidorovweb@gmail.com>
2021-10-08 14:19:44 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import './import/svg4everybody'
+import './modules/dragstart'
+import './modules/lazyLoadSVG'
+import 'focus-visible'
+
+window.addEventListener('DOMContentLoaded', () => {
+  // CODE
+})
